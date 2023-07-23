6898cfba0530e64a2b47d48c4c3a8ad6e3afa483
priya <priya1803singh@gmail.com>
2023-07-24 00:18:48 +0530

    added custom exception

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 from flask import Flask
 from src.logger import logging
+from src.exception import CustomException
+import os, sys
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET','POST'])
 def index():
-    logging.info('testing logger')
-    return 'Hello World!'
+    try:
+        raise Exception('testing')
+        logging.info('testing exception')
+        
+    except Exception as e:
+        abc = CustomException(e, sys)
+        logging.info(abc.error_message)
+        return 'Testing Custom Exception'
 
 if __name__ == '__main__':
     app.run(debug=True)
