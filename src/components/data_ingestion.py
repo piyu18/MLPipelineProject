b4acc7e06f4347418b4bf6bab6cb067a6400be14
priya <priya1803singh@gmail.com>
2023-07-30 20:56:40 +0530

    Data Ingestion Completedgit add .

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,45 @@
+import os, sys
+import pandas as pd
+import numpy as np
+from src.logger import logging
+from src.exception import CustomException
+from dataclasses import dataclass
+from sklearn.model_selection import train_test_split
+
+@dataclass
+class DataIngestionConfig:
+    train_data_path = os.path.join('artifacts', 'train.csv')
+    test_data_path = os.path.join('artifacts', 'test.csv')
+    raw_data_path = os.path.join('artifacts', 'raw.csv')
+
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config = DataIngestionConfig()
+
+    def initiate_data_ingestion(self):
+        logging.info('Starting data ingestion')
+        try:
+            logging.info('Reading data from local using pandas')
+            data = pd.read_csv(os.path.join("data", "income_cleandata.csv"))
+            #print(data.head(5))
+            #logging.info(data.head(5))
+            os.makedirs(os.path.dirname(self.ingestion_config.raw_data_path), exist_ok=True)
+            data.to_csv(self.ingestion_config.raw_data_path, index=False)
+            train_set, test_set = train_test_split(data, test_size=0.3, random_state=42)
+            train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
+            logging.info("Data ingestion completed successfully")
+
+            return (
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
+         
+
+        except Exception as e:
+            raise CustomException(e, sys)
+
+
+if __name__ == "__main__":
+    obj = DataIngestion()
+    obj.initiate_data_ingestion()
