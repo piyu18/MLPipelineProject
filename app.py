550b53cd48dc9c3c8d478a7a6e5cab9295dfc88b
priya <priya1803singh@gmail.com>
2023-08-13 02:20:15 +0530

    flask app for prediction

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, render_template, request, jsonify
+from src.pipeline.prediction_pipeline import PredictionPipeline, CustomClass
 from src.logger import logging
 from src.exception import CustomException
 import os, sys
@@ -6,15 +7,33 @@ import os, sys
 app = Flask(__name__)
 
 @app.route('/', methods=['GET','POST'])
-def index():
-    try:
-        raise Exception('testing')
-        logging.info('testing exception')
-        
-    except Exception as e:
-        abc = CustomException(e, sys)
-        logging.info(abc.error_message)
-        return 'Testing Custom Exception'
+def prediction_data():
+    if request.method == 'GET':
+        return render_template('home.html')
+    else:
+        data = CustomClass(
+            age = int(request.form.get('age')),
+            workclass = int(request.form.get('workclass')),
+            education_num = int(request.form.get('education_num')),
+            marital_status=int(request.form.get('marital_status')),
+            occupation = int(request.form.get('occupation')),
+            relationship = int(request.form.get('relationship')),
+            race = int(request.form.get('race')),
+            sex = int(request.form.get('sex')),
+            capital_gain = int(request.form.get('capital_gain')),
+            capital_loss = int(request.form.get('capital_loss')),
+            hours_per_week = int(request.form.get('hours_per_week')),
+            native_country = int(request.form.get('native_country')),
+
+        )
+        final_data = data.get_data_DataFrame()
+        pipeline_prediction = PredictionPipeline()
+        pred = pipeline_prediction.predict(final_data)
+        result = pred
+        if result == 0:
+            return render_template('results.html', final_result = "Your yearly income is less than equal to 50K:{}".format(result))
+        elif result == 1:
+            return render_template('results.html', final_result = "Your yearly income is greater than 50K:{}".format(result))
 
 if __name__ == '__main__':
     app.run(debug=True)
